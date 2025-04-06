0d15ab17a718e8a1f1f8a39a0c199f321ce6a2b6
guilhermelinsc <guilhermecalheiro@gmail.com>
2025-04-06 10:44:58 -0400

    ansible tests

--- gcp/packer/build.pkr.hcl
+++ gcp/packer/build.pkr.hcl
@@ -1,7 +1,14 @@
 build {
   sources = ["source.googlecompute.webapp"]
 
-  provisioner "shell" {
-    script = "./files/configure.sh"
+  provisioner "ansible" {
+    # galaxy_file = "./ansible/requirements.yml"
+    # galaxy_force_install = true
+
+    playbook_file    = "./ansible/playbook.yml" 
+    # ansible_env_vars = ["ANSIBLE_REMOTE_TMP=/tmp/.ansible/tmp" ]
+    user = "packer"
+    
+    extra_arguments = ["-vvvv"]
   }
 }
--- gcp/packer/files/configure.sh
+++ gcp/packer/files/configure.sh
@@ -1,8 +1,6 @@
-#! /bin/bash
-sudo apt update
-sudo apt upgrade -y
-sudo apt install apache2 net-tools -y
-#vm_hostname=\"$(curl -H "Metadata-Flavor:Google" http://169.254.169.254/computeMetadata/v1/instance/name)\"
-#echo "Page served from: $vm_hostname" | sudo tee /var/www/html/index.html
-sudo systemctl enable apache2
-sudo systemctl restart apache2
+# #! /bin/bash
+# sudo apt update
+# sudo apt upgrade -y
+# sudo apt install apache2 net-tools -y
+# sudo systemctl enable apache2
+# sudo systemctl restart apache2
